c60878f22550705a07c7cf2b28d4ca9225e797ae
Bamboo666 <chenlz991122@163.com>
2020-10-27 19:19:56 +0800

    project of ratepage

--- app/src/main/java/com/example/myapplication/ui/ratepage/RateActivity.java
+++ app/src/main/java/com/example/myapplication/ui/ratepage/RateActivity.java
@@ -1,4 +1,87 @@
 package com.example.myapplication.ui.ratepage;
 
-public class RateActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.R;
+
+public class RateActivity extends AppCompatActivity {
+    private final String TAG = "Rate";
+    private float dollarRate = 0.1f;
+    private float euroRate = 0.2f;
+    private float wonRate = 0.3f;
+
+    EditText rmb;
+    TextView show;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_rate);
+        rmb = (EditText) findViewById(R.id.rmb);
+        show = (TextView) findViewById(R.id.show);
+
+    }
+
+    public void onClick(View btn){
+        Log.i(TAG, "onClick: ");
+        String str = rmb.getText().toString();
+        Log.i(TAG, "onClick: get str=" + str);
+
+        float r = 0;
+        if(str.length()>0){
+            r = Float.parseFloat(str);
+        }else{
+            //用户没有输入内容
+            Toast.makeText(this, "请输入内容", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Log.i(TAG, "onClick: r=" + r);
+
+        if(btn.getId()==R.id.dollar){
+            show.setText(String.valueOf(r*dollarRate));
+        }else if(btn.getId()== R.id.euro){
+            show.setText(String.valueOf(r*euroRate));
+        }else{
+            show.setText(String.valueOf(r*wonRate));
+        }
+    }
+
+    public void onConfig(View btn){
+        Intent config = new Intent(this, ConfigActivity.class);
+        config.putExtra("dollar_rate_key",dollarRate);
+        config.putExtra("euro_rate_key",euroRate);
+        config.putExtra("won_rate_key",wonRate);
+
+        Log.i(TAG, "openOne: dollarRate=" + dollarRate);
+        Log.i(TAG, "openOne: euroRate=" + euroRate);
+        Log.i(TAG, "openOne: wonRate=" + wonRate);
+
+        //startActivity(config);
+        startActivityForResult(config,1);
+    }
+    public void onSave(View btn){
+        Intent config = new Intent(this,ConfigActivity.class);
+        config.putExtra("dollar_rate_key",dollarRate);
+        config.putExtra("euro_rate_key",euroRate);
+        config.putExtra("won_rate_key",wonRate);
+
+        Log.i(TAG, "openOne: dollarRate=" + dollarRate);
+        Log.i(TAG, "openOne: euroRate=" + euroRate);
+        Log.i(TAG, "openOne: wonRate=" + wonRate);
+
+        //startActivity(config);
+        startActivityForResult(config,1);
+    }
 }
